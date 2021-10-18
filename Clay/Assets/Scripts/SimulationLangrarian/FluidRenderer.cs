f7f36446e210da733b11f08da37a850ad0b50fab
Charly Yan Miller <42951087+forteller11@users.noreply.github.com>
2021-10-17 20:07:05 -0400

    debugged debug drawing of 2D simulation

---
+++ Clay/Assets/Scripts/SimulationLangrarian/FluidRenderer.cs
@@ -0,0 +1,51 @@
+﻿using ClaySimulation.Utils;
+using Shapes;
+using Sirenix.OdinInspector;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace Fort.EulerSim
+{
+    public class FluidRenderer : ImmediateModeShapeDrawer
+    {
+        [Required] [SerializeField] Simulation _simulation;
+        [TabGroup("Grid")] public float GridLineThickness = 0.1f;
+        [TabGroup("Grid")] public float GridLineSpaceThickness = 0.01f;
+
+        [TabGroup("Particles")] public float ParticleRadius = 0.1f;
+        public override void DrawShapes(Camera cam)
+        {
+            using (Draw.Command(cam))
+            {
+                Draw.Matrix = Matrix4x4.identity;
+                Draw.BlendMode = ShapesBlendMode.Opaque;
+
+                var grid = _simulation.Grid;
+                for (int i = 0; i < grid._cells.GetLength(0); i++)
+                {
+                    for (int j = 0; j < grid._cells.GetLength(1); j++)
+                    {
+                        var cell = grid._cells[i, j];
+                        var worldPos = grid.IndexToWorldPosition(new int2(i, j));
+                        var rect = new Rect(worldPos + grid._origin, grid._cellSize - GridLineSpaceThickness);
+                        
+                        Draw.Thickness = 10;
+                        Draw.Color = cell.Color.WithAlpha(1);
+                        Draw.RectangleBorder(rect, GridLineThickness);
+                    }
+                }
+                
+                var particles = _simulation.Particles;
+                for (int i = 0; i < particles.Count; i++)
+                {
+                    var part = particles[i];
+                
+                    Draw.Color = part.Color;
+                    Draw.Radius = ParticleRadius;
+                    Draw.Sphere(new float3(part.Position, 0));
+                }
+            }
+            
+        }
+    }
+}
--- Clay/Assets/Scripts/SimulationLangrarian/Simulation.cs
+++ Clay/Assets/Scripts/SimulationLangrarian/Simulation.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using ClaySimulation.Utils;
 using Shapes;
+using Sirenix.OdinInspector;
 using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Rendering;
@@ -10,42 +11,42 @@ namespace Fort.EulerSim
 {
     public class Simulation : ImmediateModeShapeDrawer
     {
-        [SerializeField] private Grid _grid;
+        public Grid Grid;
         [SerializeField] private float _viscocity = 0.98f;
         [SerializeField] private float _minDistFromCenterOfDensity = 0.001f;
 
         [SerializeField] private float2 _minMaxValidDensity = new float2(0.4f, .6f);
 
-        private List<Particle> _particles = new List<Particle>(80);
+        [NonSerialized] [ShowInInspector] public List<Particle> Particles = new List<Particle>(80);
 
         public void AddParticle(Particle particle)
         {
-            _particles.Add(particle);
+            Particles.Add(particle);
         }
 
         private void Start()
         {
-            _grid.Init();
+            Grid.Init();
         }
 
         public void Update()
         {
-            _grid.Reset();
+            Grid.Reset();
             
             #region add particles to grid
-            for (int i = 0; i < _particles.Count; i++)
+            for (int i = 0; i < Particles.Count; i++)
             {
-                _grid.AddParticleToSim(_particles[i]);
+                Grid.AddParticleToSim(Particles[i]);
             }
             #endregion
 
             #region simulate particles from grid
-            for (int i = 0; i < _particles.Count; i++)
+            for (int i = 0; i < Particles.Count; i++)
             {
-                var particle = _particles[i];
-                float2 localPos = _grid.WorldPositionToLocalPosition(particle.Position);
-                int2 cellIndex = _grid.CellIndexFromWorldPosition(particle.Position);
-                Cell cell = _grid._cells[cellIndex.x, cellIndex.y];
+                var particle = Particles[i];
+                float2 localPos = Grid.WorldPositionToLocalPosition(particle.Position);
+                int2 cellIndex = Grid.CellIndexFromWorldPosition(particle.Position);
+                Cell cell = Grid._cells[cellIndex.x, cellIndex.y];
 
                 float2 awayFromDensity = localPos - cell.CenterOfDensity;
                 particle.Velocity += awayFromDensity * 1/particle.Mass;
@@ -63,40 +64,6 @@ namespace Fort.EulerSim
                 particle.Velocity *= _viscocity;
             }
             #endregion
-        }
-
-        public override void DrawShapes(Camera cam)
-        {
-            using (Draw.Command(cam))
-            {
-                Draw.Matrix = Matrix4x4.identity;
-                Draw.BlendMode = ShapesBlendMode.Opaque;
-                
-                for (int i = 0; i < _grid._cells.GetLength(0); i++)
-                {
-                    for (int j = 0; j < _grid._cells.GetLength(1); j++)
-                    {
-                        var cell = _grid._cells[i, j];
-                        var worldPos = _grid.IndexToWorldPosition(new int2(i, j));
-                        var rect = new Rect(worldPos + _grid._origin, _grid._cellSize);
-                        Debug.Log("Start");
-                        Debug.Log(rect);
-                        Draw.Thickness = 10;
-                        Draw.Color = cell.Color.WithAlpha(1);
-                        Draw.Rectangle(rect);
-                    }
-                }
-                
-                for (int i = 0; i < _particles.Count; i++)
-                {
-                    var part = _particles[i];
-                
-                    Draw.Color = part.Color;
-                    Draw.Radius = part.Mass;
-                    Draw.Sphere(new float3(part.Position, 0));
-                }
-            }
-            
         }
     }
 }
